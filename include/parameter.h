c9b4fde1e619a028a50f8ceffad6ddd397fe5b23
YouriRigaud <rigaud.youri@proton.me>
2025-11-12 15:57:03 -0500

    UPD: parameter class remove type and index

--- include/parameter.h
+++ include/parameter.h
@@ -3,40 +3,39 @@
 // Author: Youri Rigaud
 // License: GNU GPLv3
 
+#ifndef PARAMETER_H
+#define PARAMETER_H
+
 #include <string>
 #include <variant>
 #include <vector>
 
 class Value {
     private:
-        std::variant<int, double, bool, std::string> value; // The value in a string format to deal with int, double, bool and string
+        std::variant<int, double, std::string> value; // The value in a string format to deal with int, double, bool and string
     
     public:
         Value(int val): value(val) {}
         Value(double val): value(val) {}
-        Value(bool val): value(val) {}
         Value(const std::string& val): value(val) {}
         Value(const char *val): value(std::string(val)) {}
 
         int getInt() const {return std::get<int>(value);}
         double getDouble() const {return std::get<double>(value);}
-        bool getBool() const {return std::get<bool>(value);}
         std::string getString() const {return std::get<std::string>(value);}
 };
 
 class Parameter{
-    private:
-        int index; // Index of the parameter
+    protected:
         std::string name; // Name of the parameter
-        std::string type; // Type of the parameter (int, double, bool, string)
-        Value default_value; // Default value of the parameter
         std::vector<Value> values; // All the possible values of the parameter
+        Value default_value; // Default value of the parameter
 
     public:
-        Parameter(int i, const std::string& n, const std::string& t, const Value& dv, const std::vector<Value>& vals);
-        int getIndex() const { return index; }
+        Parameter(const std::string& n, const std::vector<Value>& vals, const Value& dv): name(n), values(vals), default_value(dv) {}
         std::string getName() const { return name; }
-        std::string getType() const { return type; }
-        Value getDefaultValue() const { return default_value; }
         std::vector<Value> getValues() const { return values; }
+        Value getDefaultValue() const { return default_value; }
 };
+
+#endif // PARAMETER_H
--- src/parameter.cpp
+++ src/parameter.cpp
@@ -4,9 +4,3 @@
 // License: GNU GPLv3
 
 #include "../include/parameter.h"
-
-Parameter::Parameter(
-    int i, const std::string& n, const std::string& t,
-    const Value& dv, const std::vector<Value>& vals
-): index(i), name(n), type(t), default_value(dv), values(vals) {}
-
