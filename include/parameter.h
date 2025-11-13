61148531d476c33eb84686aaea0c14d3ea92145b
YouriRigaud <rigaud.youri@proton.me>
2025-11-13 11:45:34 -0500

    UPD: parameter and configuration attributs namestyle

--- include/parameter.h
+++ include/parameter.h
@@ -12,30 +12,38 @@
 
 class Value {
     private:
-        std::variant<int, double, std::string> value; // The value in a string format to deal with int, double, bool and string
+        std::variant<int, double, std::string> value_; // The value in a string format to deal with int, double, bool and string
     
     public:
-        Value(int val): value(val) {}
-        Value(double val): value(val) {}
-        Value(const std::string& val): value(val) {}
-        Value(const char *val): value(std::string(val)) {}
+        Value(int value): value_(value) {}
+        Value(double value): value_(value) {}
+        Value(const std::string& value): value_(value) {}
+        Value(const char *value): value_(std::string(value)) {}
 
-        int getInt() const {return std::get<int>(value);}
-        double getDouble() const {return std::get<double>(value);}
-        std::string getString() const {return std::get<std::string>(value);}
+
+        std::variant<int, double, std::string> getValue() const { return value_; }
+        std::string getString() const; // Get the value as a string
 };
 
 class Parameter{
-    protected:
-        std::string name; // Name of the parameter
-        std::vector<Value> values; // All the possible values of the parameter
-        Value default_value; // Default value of the parameter
+    private:
+        std::string name_; // Name of the parameter
+        std::vector<Value> values_; // All the possible values of the parameter
+        Value default_value_; // Default value of the parameter
 
     public:
-        Parameter(const std::string& n, const std::vector<Value>& vals, const Value& dv): name(n), values(vals), default_value(dv) {}
-        std::string getName() const { return name; }
-        std::vector<Value> getValues() const { return values; }
-        Value getDefaultValue() const { return default_value; }
+        Parameter(
+            const std::string& name,
+            const std::vector<Value>& values,
+            const Value& default_value
+        ): name_(name),
+           values_(values),
+           default_value_(default_value)
+        {}
+
+        std::string getName() const { return name_; }
+        std::vector<Value> getValues() const { return values_; }
+        Value getDefaultValue() const { return default_value_; }
 };
 
 #endif // PARAMETER_H
--- src/parameter.cpp
+++ src/parameter.cpp
@@ -4,3 +4,14 @@
 // License: GNU GPLv3
 
 #include "../include/parameter.h"
+
+std::string Value::getString() const {
+    if (std::holds_alternative<int>(value_)) {
+        return std::to_string(std::get<int>(value_));
+    } else if (std::holds_alternative<double>(value_)) {
+        return std::to_string(std::get<double>(value_));
+    } else if (std::holds_alternative<std::string>(value_)) {
+        return std::get<std::string>(value_);
+    }
+    return "";
+}
